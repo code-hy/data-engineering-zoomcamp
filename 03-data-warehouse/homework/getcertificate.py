e4e2e201f5f409a96f2a5fe2d201489d76ddf53b
Henry yao <henrysyao@gmail.com>
2026-02-23 13:02:20 +1100

    added

---
+++ 03-data-warehouse/homework/getcertificate.py
@@ -0,0 +1,16 @@
+from hashlib import sha1
+
+def compute_hash(email):
+    return sha1(email.encode('utf-8')).hexdigest()
+
+def compute_certificate_id(email):
+    email_clean = email.lower().strip()
+    return compute_hash(email_clean + '_')
+
+
+cohort = 2025
+course = 'ml-zoomcamp'
+your_id = compute_certificate_id('henrysyao@gmail.com')
+url = f"https://certificate.datatalks.club/{course}/{cohort}/{your_id}.pdf"
+print(url)
+
